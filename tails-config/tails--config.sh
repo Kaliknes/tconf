254fed040209e9fdc111178564dc8dc92cba16e9
Kaliknes <109782222+Kaliknes@users.noreply.github.com>
2023-01-21 20:28:15 +0000

    Add files via upload

---
+++ tails-config/tails--config.sh
@@ -0,0 +1,9 @@
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.background picture-uri '/home/amnesia/tails-config/parrot.jpg' #modificar path
+gsettings set org.gnome.desktop.screensaver picture-uri '/home/amnesia/tails-config/parrot.jpg'
+gsettings set org.gnome.desktop.background primary-color '#000000'
+
+cd ~/Desktop
+rm -r 'name'
+rm -r 'name2'
+cd ..
